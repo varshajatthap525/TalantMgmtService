1832c90a16018b56fd36214a90005590f3221184
varsha <varshajatthap525@gmail.com>
2024-05-03 15:28:09 +0530

    inital commit

---
+++ src/main/java/com/TelentMgmtService/TelentMgmtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.TelentMgmtService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelentMgmtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelentMgmtServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TelentMgmtService/TelentMgmtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TelentMgmtService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelentMgmtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
